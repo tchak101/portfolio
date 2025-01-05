5c06e6b241df4b696c59b713cf28ec2263f66e1f
Tathagata Chakraborty <tatu.sonali@gmail.com>
2025-01-06 04:03:11 +0530

    Hero Section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,46 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+import { HackerRoom } from "../components/HackerRoom";
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, I am Tathagata <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+      <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            {/* To hide controller */}
+            <Leva hidden />
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+
+            <HeroCamera isMobile={isMobile}>
+              <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
+            </HeroCamera>
+
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Rings position={sizes.ringPosition} />
+              <Cube position={sizes.cubePosition} />
+            </group>
+
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
