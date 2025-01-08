a8d658763ef7656425d974f265c470c1fc0e0084
Tathagata Chakraborty <tatu.sonali@gmail.com>
2025-01-09 04:41:07 +0530

    Hero section

--- src/components/HackerRoom.jsx
+++ src/components/HackerRoom.jsx
@@ -5,12 +5,12 @@ export function HackerRoom(props) {
   const { nodes, materials } = useGLTF('/models/hacker-room.glb');
 
   const monitortxt = useTexture('textures/desk/monitor.png');
-  const screenTxt = useTexture('textures/desk/screen.png');
+  // const screenTxt = useTexture('textures/desk/screen.png');
 
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.screen_screens_0.geometry} material={materials.screens}>
-        <meshMatcapMaterial map={screenTxt} />
+        {/* <meshMatcapMaterial map={screenTxt} /> */}
       </mesh>
       <mesh geometry={nodes.screen_glass_glass_0.geometry} material={materials.glass} />
       <mesh geometry={nodes.table_table_mat_0_1.geometry} material={materials.table_mat} />
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,26 +1,29 @@
-
-import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { useMediaQuery } from 'react-responsive';
-import { PerspectiveCamera } from '@react-three/drei';
-import CanvasLoader from '../components/CanvasLoader.jsx';
-import { HackerRoom } from '../components/HackerRoom.jsx';
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+import { PerspectiveCamera } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import { HackerRoom } from "../components/HackerRoom.jsx";
 
 // import Cube from '../components/Cube.jsx';
 // import Rings from '../components/Rings.jsx';
 // import ReactLogo from '../components/ReactLogo.jsx';
 // import Button from '../components/Button.jsx';
-import Target from '../components/Target.jsx';
+import Target from "../components/Target.jsx";
 // import HeroCamera from '../components/HeroCamera.jsx';
-import { calculateSizes } from '../constants/index.js';
+import { calculateSizes } from "../constants/index.js";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Rings from "../components/Rings.jsx";
+import Cube from "../components/Cube.jsx";
+import HeroCamera from "../components/HeroCamera.jsx";
+import Button from "../components/Button.jsx";
 const Hero = () => {
-
   const isSmall = useMediaQuery({ maxWidth: 440 });
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
-  
+
   return (
     <section className="relative flex flex-col w-full min-h-screen" id="home">
       <div className="flex flex-col w-full gap-3 mx-auto mt-20 sm:mt-36 c-space">
@@ -33,36 +36,37 @@ const Hero = () => {
       </div>
 
       <div className="absolute inset-0 w-full h-full">
-      {/* <Leva /> */}
-      <Canvas className="w-full h-full">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             {/* <Leva hidden /> */}
             <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-            <HackerRoom 
-             position={sizes.deskPosition}
-             scale={sizes.deskScale}
-             rotation={[0, -Math.PI, 0]}
-            />
 
-            {/* <HeroCamera isMobile={isMobile}>
-              <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
-            </HeroCamera> */}
+            <HeroCamera isMobile={isMobile}>
+              <HackerRoom
+                position={sizes.deskPosition}
+                scale={sizes.deskScale}
+                rotation={[0, -Math.PI, 0]}
+              />
+            </HeroCamera>
 
             <group>
-            <Target position={sizes.targetPosition} />
-            </group>
-
-            {/* <group>
               <Target position={sizes.targetPosition} />
               <ReactLogo position={sizes.reactLogoPosition} />
               <Rings position={sizes.ringPosition} />
               <Cube position={sizes.cubePosition} />
-            </group> */}
+            </group>
 
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
+      </div>
+
+      <div className="absolute left-0 right-0 z-10 w-full bottom-7 c-space">
+        <a href="#about" className="w-fit">
+          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+        </a>
       </div>
     </section>
   );
