06c8484a153a4856f5ec4f293fd7a50e91f4d317
Tathagata Chakraborty <tatu.sonali@gmail.com>
2025-01-12 12:59:55 +0530

    contact form and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import About from "./sections/About"
+import Clients from "./sections/Clients"
+import Contact from "./sections/Contact"
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
+import Projects from "./sections/Projects"
 
 
 const App = () => {
@@ -9,6 +12,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
+      <Clients />
+     <Contact />
     </main>
   )
 }
---
+++ src/sections/Contact.jsx
@@ -0,0 +1,131 @@
+import { EmailJSResponseStatus } from "@emailjs/browser";
+import { useState } from "react";
+import { useRef } from "react";
+
+const Contact = () => {
+
+  const formRef = useRef();
+
+  const { alert, showAlert, hideAlert } = useAlert();
+  const [loading, setLoading] = useState(false);
+
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    EmailJSResponseStatus
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'JavaScript Mastery',
+          from_email: form.email,
+          to_email: 'sujata@jsmastery.pro',
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+      )
+      .then(
+        () => {
+          setLoading(false);
+          showAlert({
+            show: true,
+            text: 'Thank you for your message 😃',
+            type: 'success',
+          });
+
+          setTimeout(() => {
+            hideAlert(false);
+            setForm({
+              name: '',
+              email: '',
+              message: '',
+            });
+          }, [3000]);
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          showAlert({
+            show: true,
+            text: "I didn't receive your message 😢",
+            type: 'danger',
+          });
+        },
+      );
+  };
+
+
+  
+  return (
+    <section className="my-20 c-space" id="contact">
+      <div className="relative flex flex-col items-center justify-center min-h-screen">
+      <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
+        <div className="contact-container">
+        <h3 className="head-text">Let's talk</h3>
+          <p className="mt-3 text-lg text-white-600">
+            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
+            life, I’m here to help.
+          </p>
+
+          <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col mt-12 space-y-7">
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., John Doe"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Email address</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., johndoe@gmail.com"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Share your thoughts or inquiries..."
+              />
+            </label>
+
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? 'Sending...' : 'Send Message'}
+
+              <img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow" />
+            </button>
+            </form>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
